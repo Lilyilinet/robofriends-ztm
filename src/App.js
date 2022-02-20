7db8a81d1db73a36e217ad759d02a9e989a56d5a
lily <59627575+Lilyilinet@users.noreply.github.com>
2022-02-20 13:13:34 +0300

    work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-// import CardList from './CardList';
+import CardList from './CardList';
 import SearchBox from './SearchBox';
+import { robots } from './robots'
 
 
 
@@ -8,37 +9,27 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: [],
+      robots: robots,
       searchfield: ''
     }
   }
 
- componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
-  }
-		
-onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
+  onSearchChange = (event) => {
+    this.setState({searchfield:event.target.value})
   }
 
-  render() {
-  	const {robots, searchfield } = this.state;
-  	const filteredRobots = robots.filter(robot =>{
-  	return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+ render() {
+  const filteredRobots = this.state.robots.filter(robots =>{
+    return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
   })
-  return !robots.length ?
-  <h1>Loading</h1> :
-   (
-	  <div className='tc'>
-		<h1 className='f1'>RoboFriends</h1>
-		<SearchBox searchChange={this.onSearchChange}/>
-	  </div>
-    );
-   }
+   return (
+     <div className='tc'>
+       <h1>Robofriends</h1>
+       <SearchBox searchChange={this.onSearchChange}/>
+       <CardList robots={filteredRobots} />
+     </div>
+   );
  }
-
-
+}
 export default App;
 
